5e55747ef7f66ddfb595dfd554bc6944e4f8136a
Silicon <silicons@163.com>
2017-03-03 14:08:55 +0800

    add files

---
+++ Model/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  Lacamomille
+//
+//  Created by Silicon.Zou on 2017/3/3.
+//  Copyright © 2017年 Silicon.Zou. All rights reserved.
+//
+
+import UIKit
+
+class DataModel: NSObject {
+
+}
